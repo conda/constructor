156bb73c200a9a7990148b2953a07e6d75d17c5a
Ilan Schnell <ilanschnell@gmail.com>
2016-02-26 10:08:27 -0600

    better naming

--- constructor/winexe.py
+++ constructor/winexe.py
@@ -36,7 +36,7 @@ def read_nsi_tmpl():
         return fi.read()
 
 
-def find_msvc_runtimes(dists, py_version):
+def find_vs_runtimes(dists, py_version):
     vs_map = {'2.7': 'vs2008_runtime',
               '3.4': 'vs2010_runtime',
               '3.5': 'vs2015_runtime'}
@@ -79,12 +79,12 @@ def make_nsi(info, dir_path):
     data = data.replace('@NSIS_DIR@', NSIS_DIR)
     data = data.replace('@BITS@', str(arch))
 
-    msvc_dists = find_msvc_runtimes(dists, py_version)
-    if len(msvc_dists) != 1:
-        sys.exit("Error: MSVC runtimes found: %s" % msvc_dists)
+    vs_dists = find_vs_runtimes(dists, py_version)
+    if len(vs_dists) != 1:
+        sys.exit("Error: MSVC runtimes found: %s" % vs_dists)
 
     pkg_commands = []
-    for n, fn in enumerate(msvc_dists + dists):
+    for n, fn in enumerate(vs_dists + dists):
         path = join(info['_download_dir'], fn)
         pkg_commands.append('# --> %s <--' % fn)
         pkg_commands.append('File %s' % str_esc(path))
