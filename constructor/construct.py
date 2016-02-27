4ba7a12eccb8ce040e82b9ed5e90c9ae59a4596b
Ilan Schnell <ilanschnell@gmail.com>
2016-02-27 00:17:32 -0600

    add descriptions for all keys in construct.yaml

--- constructor/construct.py
+++ constructor/construct.py
@@ -12,22 +12,85 @@ import yaml
 import conda.config
 
 
+# list of tuples (key name, required, type, description)
 KEYS = [
-    'name',
-    'version',
-    'channels',
-    'specs',
-    'exclude',
-    'packages',
-    'sort_by_name',
-    'platform',
-    'conda_default_channels',
-    'installer_filename',
-    'license_file',
-    'default_prefix',
-    'welcome_image',
-    'header_image',
-    'icon_image',
+    ('name',                   True,  str, '''
+Name of the installer.  May also contain uppercase letter.  The installer
+name is independent of the names of any of the conda packages the installer
+is composed of.
+'''),
+
+    ('version',                True,  str, '''
+Version of the installer.  Just like the installer name, this version
+is independent of any conda package versions contained in the installer.
+'''),
+
+    ('channels',               False, list, '''
+The conda channels from which packages are retrieved, when using the `specs`
+key below, but also when using the `packages` key ,unless the full URL is
+given in the `packages` list (see below).
+'''),
+
+    ('specs',                  False, (list, str), '''
+List of package specifications, e.g. `python 2.7*`, `pyzmq` or `numpy >=1.8`.
+This list of specifications if given to the conda resolver (as if you were
+to create a new environment with those specs.
+'''),
+
+    ('exclude',                False, list, '''
+List of package names to be excluded, after the '`specs` have been resolved.
+For example, you can say that `readline` should be excluded, even though it
+is contained as a result of resolving the specs for `python 2.7`.
+'''),
+
+    ('packages',               False, (list, str), '''
+A list of explicit conda packages to be included, eg. `yaml-0.1.6-0.tar.bz2`.
+The packages may also be specified by their entire URL,
+eg.`https://repo.continuum.io/pkgs/free/osx-64/openssl-1.0.1k-1.tar.bz2`.
+Optionaly, the MD5 hash sum of the package, may be added after an immediate
+`#` character, eg. `readline-6.2-2.tar.bz2#0801e644bd0c1cd7f0923b56c52eb7f7`.
+'''),
+
+    ('sort_by_name',           False, bool, '''
+By default packages are sorted by install dependency order (unless the
+explicit list in `packages` is used.  Python is always moved to the front
+of the packages to be installed.  This option allows sorting by the package
+names instead.
+'''),
+
+    ('platform',               False, str, '''
+The platform for which the installer is created, eg. `linux-32`.  This is
+not necessarily the current platform.  The default, however, is the current
+platform.
+'''),
+
+    ('conda_default_channels', False, list, 'XXX'),
+
+    ('installer_filename',     False, str, '''
+The filename of the installer being created.  A reasonable default filename
+will determined by the `name`, `version`, `platform` and installer type.
+'''),
+
+    ('license_file',           False, str, '''
+Path to the license file being displayed by the installer during the install
+process.
+'''),
+
+    ('default_prefix',         False, str, 'XXX'),
+
+    ('welcome_image',          False, str, '''
+Path to an image which is used as the welcome image for the Windows
+installer.  The image is resized to 164 x 314 pixels.
+By default, an image is automatically generated.
+'''),
+
+    ('header_image',           False, str, '''
+Like `welcome_image` for Windows, resized to 150 x 57 pixels.
+'''),
+
+    ('icon_image',             False, str, '''
+Like `welcome_image` for Windows, resized to 256 x 256 pixels.
+'''),
 ]
 
 
@@ -75,11 +138,20 @@ def parse(path):
 
 
 def verify(info):
-    for key in info:
-        if key not in KEYS:
-            sys.exit("Error: unknown key '%s' in construct.yaml" % key)
+    types_key = {} # maps key to types
+    required_keys = set()
+    for key, required, types, unused_descr in KEYS:
+        types_key[key] = types
+        if required:
+            required_keys.add(key)
 
-    for key in 'name', 'version':
+    for key in info:
+        if key not in types_key:
+            sys.exit("Error: unknown key '%s' in construct.yaml" % key)
+        if not isinstance(info[key], types_key[key]):
+            sys.exit("Error: key '%s' points to wrong type" % key)
+
+    for key in required_keys:
         if key not in info:
             sys.exit("Error: Required key '%s' not found in construct.yaml" %
                      key)
