16583ddc7138321b30a8035241c20a5e451cead4
Ilan Schnell <ilanschnell@gmail.com>
2016-02-29 16:17:38 -0600

    work on setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 # constructor is distributed under the terms of the BSD 3-clause license.
 # Consult LICENSE.txt or http://opensource.org/licenses/BSD-3-Clause.
 
+import re
 import sys
 from os.path import abspath, join
 
@@ -12,13 +13,20 @@ if 'develop' in sys.argv:
 from distutils.core import setup
 
 
+# read version from constructor/__init__.py
+pat = re.compile(r'__version__\s*=\s*(\S+)', re.M)
+data = open(join('constructor', '__init__.py')).read()
+version = eval(pat.search(data).group(1))
+
 setup(
     name = "constructor",
-    version = "0.9.3",
+    version = version,
     author = "Ilan Schnell",
     author_email = "ilan@continuum.io",
+    url = "https://github.com/conda/constructor",
     license = "BSD",
     description = "create installer from conda packages",
+    long_description = open('README.md').read(),
     packages = ['constructor', 'constructor.tests'],
     scripts = ['bin/constructor'],
 )
