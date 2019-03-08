3441daeb51d4f61f0e3e3073ac2c421aa080bc32
Stefan Scherfke <stefan.scherfke@energymeteo.de>
2019-03-08 15:38:21 +0100

    Fix "conda init" section for shell configs

--- constructor/header.sh
+++ constructor/header.sh
@@ -514,21 +514,20 @@ if [ "$BATCH" = "0" ]; then
         cat <<EOF >> "$BASH_RC"
 
 # added by __NAME__ __VERSION__ installer
-# >>> conda init >>>
+# >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="\$(CONDA_REPORT_ERRORS=false '$PREFIX/bin/conda' shell.bash hook 2> /dev/null)"
+__conda_setup="\$('$PREFIX/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ \$? -eq 0 ]; then
-    \\eval "\$__conda_setup"
+    eval "\$__conda_setup"
 else
     if [ -f "$PREFIX/etc/profile.d/conda.sh" ]; then
         . "$PREFIX/etc/profile.d/conda.sh"
-        CONDA_CHANGEPS1=false conda activate base
     else
-        \\export PATH="$PREFIX/bin:\$PATH"
+        export PATH="$PREFIX/bin:\$PATH"
     fi
 fi
 unset __conda_setup
-# <<< conda init <<<
+# <<< conda initialize <<<
 EOF
         printf "\\n"
         printf "For this change to become active, you have to open a new terminal.\\n"
