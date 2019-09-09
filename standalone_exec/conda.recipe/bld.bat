aaea40cefaea7c77ed72b56ae2ad613e42d564f5
Mike Sarahan <msarahan@gmail.com>
2019-09-09 16:10:01 -0500

    win build fixes

--- standalone_exec/conda.recipe/bld.bat
+++ standalone_exec/conda.recipe/bld.bat
@@ -1,4 +1,9 @@
-:: -F is to create a single file
+COPY conda_src\conda\core\path_actions.py %SP_DIR%\conda\core\path_actions.py
+COPY conda_src\conda\utils.py %SP_DIR%\conda\utils.py
 
+COPY menuinst_src\menuinst\__init__.py %SP_DIR%\menuinst\__init__.py
+COPY menuinst_src\menuinst\win32.py %SP_DIR%\menuinst\win32.py
+
+:: -F is to create a single file
 pyinstaller -F -n conda.exe conda.exe.py
-MOVE work/dist/conda.exe %PREFIX%/conda-%conda_version%.exe
+MOVE dist\conda.exe %PREFIX%\conda-%conda_version%.exe
--- standalone_exec/conda.recipe/build.sh
+++ standalone_exec/conda.recipe/build.sh
@@ -1,5 +1,11 @@
-rsync -avz --update --existing conda_src/conda $SP_DIR/conda
-rsync -avz --update --existing menuinst_src/menuinst $SP_DIR/menuinst
+# patched conda files
+cp conda_src/conda/core/path_actions.py $SP_DIR/conda/core/path_actions.py
+cp conda_src/conda/utils.py $SP_DIR/conda/utils.py
+
+# # patched menuinst files - windows only, so ignore these
+# cp menuinst_src/menuinst/__init__.py ${SP_DIR}/menuinst/__init__.py
+# cp menuinst_src/menuinst/win32.py ${SP_DIR}/menuinst/win32.py
+
 # -F is to create a single file
 # -s strips executables and libraries
 pyinstaller -F -s -n conda.exe conda.exe.py
