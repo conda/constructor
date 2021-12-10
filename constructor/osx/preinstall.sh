3e110b27de3b001eff28cfd5fbf53d775ec5aebb
Jaime RGP <jaimergp@users.noreply.github.com>
2021-12-10 03:21:56 +0100

    better style

--- constructor/osx/preinstall.sh
+++ constructor/osx/preinstall.sh
@@ -7,8 +7,10 @@ if [[ -e "$2/__NAME_LOWER__" ]]; then
     # By default, osascript doesn't allow user interaction, so we have to work
     # around it.  http://stackoverflow.com/a/11874852/161801
     (osascript -e "try
-tell application \"Installer\"
-display alert \"Path $2/__NAME_LOWER__ already exists. Please, relaunch the installer and choose another location in the Destination Select step.\" buttons {\"OK\"} default button {\"OK\"}
+tell application (path to frontmost application as text)
+set theAlertText to \"Chosen path already exists!\"
+set theAlertMessage to \"'$2/__NAME_LOWER__' already exists. Please, relaunch the installer and choose another location in the Destination Select step.\"
+display alert theAlertText message theAlertMessage as critical buttons {\"OK\"} default button {\"OK\"}
 end
 activate app (path to frontmost application as text)
 answer
@@ -20,8 +22,10 @@ fi
 case "$2" in
      *\ * )
            (osascript -e "try
-tell application \"Installer\"
-display alert \"__NAME__ cannot be installed in $2 because the path contains spaces. Please, relaunch the installer and choose another location in the Destination Select step.\" buttons {\"OK\"} default button {\"OK\"}
+tell application (path to frontmost application as text)
+set theAlertText to \"Chosen path contain spaces!\"
+set theAlertMessage to \"'$2/__NAME_LOWER__' contains spaces. Please, relaunch the installer and choose another location in the Destination Select step.\"
+display alert theAlertText message theAlertMessage as critical buttons {\"OK\"} default button {\"OK\"}
 end
 activate app (path to frontmost application as text)
 answer
