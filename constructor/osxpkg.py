7b718d048a207fc35d5c6b67d5dff2e161146f6f
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-08-15 09:40:29 +0200

    one more path and some comments

--- constructor/osxpkg.py
+++ constructor/osxpkg.py
@@ -374,8 +374,9 @@ def create(info, verbose=False):
             }
             plist_dump(plist, f)
         check_call(
-            [
-                "/usr/bin/codesign",
+            [   
+                # hardcode to system location to avoid accidental clobber in PATH
+                "/usr/bin/codesign",  
                 "--verbose",
                 '--sign', notarization_identity_name,
                 "--prefix", info.get("reverse_domain_identifier", info['name']),
@@ -411,7 +412,8 @@ def create(info, verbose=False):
     # The default distribution file needs to be modified, so we create
     # it to a temporary location, edit it, and supply it to the final call.
     xml_path = join(PACKAGES_DIR, 'distribution.xml')
-    args = ["productbuild", "--synthesize"]
+    # hardcode to system location to avoid accidental clobber in PATH
+    args = ["/usr/bin/productbuild", "--synthesize"]
     for name in names:
         args.extend(['--package', join(PACKAGES_DIR, "%s.pkg" % name)])
     args.append(xml_path)
@@ -428,6 +430,7 @@ def create(info, verbose=False):
     ])
     if identity_name:
         check_call([
+            # hardcode to system location to avoid accidental clobber in PATH
             '/usr/bin/productsign', '--sign', identity_name,
             "tmp.pkg",
             info['_outpath'],
