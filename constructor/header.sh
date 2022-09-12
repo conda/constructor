98eb2723c530246a20f9e0ccaeb58550ef822306
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-09-12 17:30:57 +0200

    Make pre/post install scripts UX more consistent (#558)
    * add name, version and platform env vars for scripts in windows
    
    * add support for preinstall scripts on Windows
    
    * add examples
    
    * remove block on windows
    
    * warning in pkg
    
    * add more tests (undefined vars)
    
    * add osxpkg wrapping logic to define some useful variables
    
    * add some more tests
    
    * mark post install as "user provided"
    
    * update docs
    
    * add support for preinstall scripts on PKGs too
    
    * fix how we run the pre_install script, and how INSTALLER_PLAT is defined on Windows
    
    * no .get
    
    * missing argument in macro
    
    * fix preinstall prints
    
    * break tests on purpose
    
    * revert breakage; this will work now
    
    * expose INSTALLER_TYPE for windows too
    
    * refine docs
    
    * add news
    
    * Make preinstall script on mac selectable
    
    * add 'pre_install_desc' on Windows too
    
    * add missing key
    
    * windows: fit checkboxes properly
    
    * hide 'run_installation' checkbox
    
    * update news
    
    * all calls to AbortRetryNSExecWait must specify WithLog/NoLog
    
    * clarify pre|post_install_desc docs for shell installers

--- constructor/header.sh
+++ constructor/header.sh
@@ -18,9 +18,11 @@ if ! echo "$0" | grep '\.sh$' > /dev/null; then
 fi
 
 # Export variables to make installer metadata available to pre/post install scripts
+# NOTE: If more vars are added, make sure to update the examples/scripts tests too
 export INSTALLER_NAME="__NAME__"
 export INSTALLER_VER="__VERSION__"
 export INSTALLER_PLAT="__PLAT__"
+export INSTALLER_TYPE="SH"
 
 THIS_DIR=$(DIRNAME=$(dirname "$0"); cd "$DIRNAME"; pwd)
 THIS_FILE=$(basename "$0")
---
+++ examples/scripts/pre_install.bat
@@ -0,0 +1,6 @@
+if not "%INSTALLER_NAME%" == "Scripts" exit 1
+if not "%INSTALLER_VER%" == "X" exit 1
+if not "%INSTALLER_PLAT%" == "win-64" exit 1
+if not "%INSTALLER_TYPE%" == "EXE" exit 1
+if "%PREFIX%" == "" exit 1
+echo Added by pre-install script > "%PREFIX%\pre_install_sentinel.txt"
