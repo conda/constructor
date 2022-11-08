d633f2234618c02e444de727622a1f8948729813
Paul Yim <paul.j.yim@gmail.com>
2022-11-07 19:22:03 -0500

    Add extra_pages_file to list of keys to expand full path

--- constructor/main.py
+++ constructor/main.py
@@ -94,7 +94,7 @@ def main_build(dir_path, output_dir='.', platform=cc_platform,
     for key in ('license_file', 'welcome_image', 'header_image', 'icon_image',
                 'pre_install', 'post_install', 'pre_uninstall', 'environment_file',
                 'nsis_template', 'welcome_file', 'readme_file', 'conclusion_file',
-                'signing_certificate'):
+                'signing_certificate', 'extra_pages_file'):
         if info.get(key):  # only join if there's a truthy value set
             info[key] = abspath(join(dir_path, info[key]))
 
