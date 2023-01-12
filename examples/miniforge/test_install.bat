496518c343ab7004e5d793a7b9f6e2eef1a9af5e
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2023-01-12 22:14:13 +0100

    use double quotes just in case

--- examples/miniforge/test_install.bat
+++ examples/miniforge/test_install.bat
@@ -2,6 +2,6 @@
 call "%PREFIX%\Scripts\activate.bat
 conda install -yq jq || exit 1
 conda config --show-sources || exit 1
-conda config --json --show | jq -r '.channels[0]' > temp.txt
+conda config --json --show | jq -r ".channels[0]" > temp.txt
 set /p OUTPUT=<temp.txt
 if not "%OUTPUT%" == "conda-forge" exit 1
