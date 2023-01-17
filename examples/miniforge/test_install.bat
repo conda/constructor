77e1b051fc92396cd3f7a9c5804306260dc99c1a
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2023-01-17 19:09:48 +0100

    EnableDelayedExpansion

--- examples/miniforge/test_install.bat
+++ examples/miniforge/test_install.bat
@@ -1,4 +1,5 @@
 echo Added by test-install script > "%PREFIX%\test_install_sentinel.txt"
+SetLocal EnableDelayedExpansion
 
 @ECHO ON
 call "%PREFIX%\Scripts\activate.bat
@@ -6,4 +7,4 @@ conda install -yq jq || exit 1
 conda config --show-sources || exit 1
 conda config --json --show | jq -r ".channels[0]" > temp.txt
 set /p OUTPUT=<temp.txt
-if not "%OUTPUT%" == "conda-forge" exit 1
+if not "!OUTPUT!" == "conda-forge" exit 1
