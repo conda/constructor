84f512044a52849133adc358dee92ac6c1b60b52
jaimergp <jaimergp@users.noreply.github.com>
2023-07-07 13:51:41 +0200

    pre-commit

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -316,7 +316,7 @@ def test_example_extra_files(tmp_path, request):
 
 
 @pytest.mark.skipif(
-    sys.platform == "Darwin" and CONSTRUCTOR_CONDA_EXE.endswith("micromamba"), 
+    sys.platform == "Darwin" and CONSTRUCTOR_CONDA_EXE.endswith("micromamba"),
     reason="Known to fail. See https://github.com/conda/constructor/issues/674."
 )
 def test_example_miniforge(tmp_path, request):
