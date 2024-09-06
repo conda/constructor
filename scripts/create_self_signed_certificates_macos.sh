ca9f855e6cebac0e0f81d99cf2f76567c721130d
Marco Esters <mesters@anaconda.com>
2024-09-06 10:44:24 -0700

    Test: Skip trusting

--- scripts/create_self_signed_certificates_macos.sh
+++ scripts/create_self_signed_certificates_macos.sh
@@ -69,11 +69,11 @@ for context in ${APPLICATION_ROOT} ${INSTALLER_ROOT}; do
     # Output to verify installer signatures
     fingerprint=$(openssl x509 -in "${pemfile}" -noout -fingerprint -sha256 | cut -f2 -d'=' | sed 's/://g')
     echo "SHA256 ${commonname} = ${fingerprint}"
-    if [[ "${context}" == "installer" ]] && [[ -z "${CI}" ]]; then
-        # Installer certificates must be trusted to be found in the keychain.
-        # In non-CI environments, users will be asked for a passkey.
-        security add-trusted-cert -p basic -k "${KEYCHAIN_PATH}" "${pemfile}"
-    fi
+    #if [[ "${context}" == "installer" ]] && [[ -z "${CI}" ]]; then
+    #    # Installer certificates must be trusted to be found in the keychain.
+    #    # In non-CI environments, users will be asked for a passkey.
+    #    security add-trusted-cert -p basic -k "${KEYCHAIN_PATH}" "${pemfile}"
+    #fi
 done
 
 # Add keychain at the beginning of the keychain list
