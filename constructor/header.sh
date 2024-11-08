138616e772a432fc470f8f780f3b43f92355299b
jaimergp <jaimergp@users.noreply.github.com>
2024-11-08 18:07:00 +0100

    move right before the first mkdir

--- constructor/header.sh
+++ constructor/header.sh
@@ -21,16 +21,6 @@ if ! echo "$0" | grep '\.sh$' > /dev/null; then
     return 1
 fi
 
-total_installation_size_kb="__TOTAL_INSTALLATION_SIZE_KB__"
-free_disk_space_bytes="$(df -Pk "$PREFIX" | tail -n 1 | awk '{print $4}')"
-free_disk_space_kb="$((free_disk_space_bytes / 1024))"
-free_disk_space_kb_with_buffer="$((free_disk_space_kb - 50 * 1024))"  # add 50MB of buffer
-if [ "$free_disk_space_kb_with_buffer" -lt "$total_installation_size_kb" ]; then
-    printf "ERROR: Not enough free disk space. Only %s MB are available, but %s MB are required (leaving a 50 MB buffer).\\n" \
-        "$((free_disk_space_kb / 1024))" "$((total_installation_size_kb / 1024))" >&2
-    exit 1
-fi
-
 #if osx and min_osx_version
 min_osx_version="__MIN_OSX_VERSION__"
 system_osx_version=$(SYSTEM_VERSION_COMPAT=0 sw_vers -productVersion)
@@ -397,6 +387,16 @@ if [ "$FORCE" = "0" ] && [ -e "$PREFIX" ]; then
     exit 1
 elif [ "$FORCE" = "1" ] && [ -e "$PREFIX" ]; then
     REINSTALL=1
+fi
+
+total_installation_size_kb="__TOTAL_INSTALLATION_SIZE_KB__"
+free_disk_space_bytes="$(df -Pk "$PREFIX" | tail -n 1 | awk '{print $4}')"
+free_disk_space_kb="$((free_disk_space_bytes / 1024))"
+free_disk_space_kb_with_buffer="$((free_disk_space_kb - 50 * 1024))"  # add 50MB of buffer
+if [ "$free_disk_space_kb_with_buffer" -lt "$total_installation_size_kb" ]; then
+    printf "ERROR: Not enough free disk space. Only %s MB are available, but %s MB are required (leaving a 50 MB buffer).\\n" \
+        "$((free_disk_space_kb / 1024))" "$((total_installation_size_kb / 1024))" >&2
+    exit 1
 fi
 
 if ! mkdir -p "$PREFIX"; then
