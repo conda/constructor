b4de46060e32a90d55ee266ac54f06e1db289961
Julien Jerphanion <git@jjerphan.xyz>
2025-01-10 17:59:38 +0100

    test: Add test for mamba 2.0.5
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>
    
    Co-authored-by: jaimergp <jaimergp@users.noreply.github.com>

--- examples/miniforge/test_install.sh
+++ examples/miniforge/test_install.sh
@@ -15,6 +15,31 @@ conda info -v
 echo "+ conda config"
 conda config --show-sources
 
-echo "+ Testing channels"
+echo "+ Testing conda channels"
 conda config --show --json | python -c "import sys, json; info = json.loads(sys.stdin.read()); assert 'conda-forge' in info['channels'], info"
+echo "  OK"
+
+echo "+ Testing mamba installation"
+mamba --version
+echo "  OK"
+
+echo "+ mamba info"
+mamba info
+
+echo "+ mamba config sources"
+mamba config sources
+
+echo "+ mamba config list"
+mamba config list
+
+echo "+ Testing mamba 2 version"
+mamba info --json | python -c "import sys, json; info = json.loads(sys.stdin.read()); assert info['mamba version'] == '2.0.5', info"
+echo "  OK"
+
+echo "+ Testing libmambapy 2 version"
+python -c "import libmambapy; assert libmambapy.__version__ == '2.0.5', f'libmamba version got: {libmambapy.__version__}; expected: 2.0.5'"
+echo "  OK"
+
+echo "+ Testing mamba channels"
+mamba info --json | python -c "import sys, json; info = json.loads(sys.stdin.read()); assert any('conda-forge' in c for c in info['channels']), info"
 echo "  OK"
