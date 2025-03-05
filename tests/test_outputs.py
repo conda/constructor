3b39bc307158f184d9bdfbcad2e5bfc34f7b6b51
Marco Esters <mesters@anaconda.com>
2025-03-05 09:07:44 -0800

    Enforce LF line endings for checksum files (#938)
    * Enforce POSIX newlines for checksum files
    
    * Add news
    
    * Refactor tests to cover all input types
    
    * Assert no carriage return in checksum file
    
    * Use consistent string formatting
    
    * Make writing compatible with python 3.9
    
    * Add missing mode
    
    * Add comment about POSIX line endings on Windows to documentation

--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -1,38 +1,52 @@
+from contextlib import nullcontext
 from pathlib import Path
 
 import pytest
 
 from constructor.build_outputs import dump_hash
 
+TEST_FILES = {
+    "test.txt": {
+        "content": "test string",
+        "sha256": "d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b",
+        "md5": "6f8db599de986fab7a21625b7916589c",
+    },
+    "test2.txt": {
+        "content": "another test",
+        "sha256": "64320dd12e5c2caeac673b91454dac750c08ba333639d129671c2f58cb5d0ad1",
+        "md5": "5e8862cd73694287ff341e75c95e3c6a",
+    },
+}
 
-def test_hash_dump(tmp_path):
-    testfile = tmp_path / "test.txt"
-    testfile.write_text("test string")
-    testfile = tmp_path / "test2.txt"
-    testfile.write_text("another test")
-    expected = {
-        "sha256": (
-            "d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b",
-            "64320dd12e5c2caeac673b91454dac750c08ba333639d129671c2f58cb5d0ad1",
-        ),
-        "md5": (
-            "6f8db599de986fab7a21625b7916589c",
-            "5e8862cd73694287ff341e75c95e3c6a",
-        ),
-    }
+
+@pytest.mark.parametrize(
+    "algorithm,context",
+    (
+        pytest.param("bad algorithm", pytest.raises(ValueError), id="invalid algorithm"),
+        pytest.param("sha256", nullcontext(), id="string"),
+        pytest.param(["sha256", "md5"], nullcontext(), id="list"),
+    ),
+
+)
+def test_hash_dump(tmp_path, algorithm, context):
     info = {
-        "_outpath": [
-            str(tmp_path / "test.txt"),
-            str(tmp_path / "test2.txt"),
-        ]
+        "_outpath": []
     }
-    with pytest.raises(ValueError):
-        dump_hash(info, algorithm="bad_algorithm")
-    dump_hash(info, algorithm=["sha256", "md5"])
-    for f, file in enumerate(info["_outpath"]):
-        for algorithm in expected:
-            hashfile = Path(f"{file}.{algorithm}")
-            assert hashfile.exists()
-            filehash, filename = hashfile.read_text().strip().split()
-            assert filehash == expected[algorithm][f]
-            assert filename == Path(file).name
+    for file, data in TEST_FILES.items():
+        testfile = tmp_path / file
+        testfile.write_text(data["content"])
+        info["_outpath"].append(str(testfile))
+    with context:
+        dump_hash(info, algorithm=algorithm)
+        if isinstance(algorithm, str):
+            algorithm = [algorithm]
+        for file in info["_outpath"]:
+            for algo in algorithm:
+                hashfile = Path(f"{file}.{algo}")
+                assert hashfile.exists()
+                with open(hashfile, newline="") as f:
+                    content = f.read()
+                assert "\r" not in content
+                filehash, filename = content.strip().split()
+                assert filename == Path(file).name
+                assert filehash == TEST_FILES[filename][algo]
