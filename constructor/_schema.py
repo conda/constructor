be1758f4e85b6506b4b62fc10c9ce7db52883bef
Isabel Paredes <iparedes314@gmail.com>
2025-07-07 11:29:02 +0200

    Include mamba in description
    Co-authored-by: jaimergp <jaimergp@users.noreply.github.com>

--- constructor/_schema.py
+++ constructor/_schema.py
@@ -245,7 +245,11 @@ class ConstructorConfiguration(BaseModel):
     mirrored_channels: dict[NonEmptyStr, list[NonEmptyStr]] = {}
     """
     A mapping of channels to their mirror URLs. Each channel maps to a list of
-    mirror URLs that will be used as fallbacks. Example use:
+    mirror URLs that will be used as fallbacks.
+    
+    Requires `mamba` in the `base` environment.
+    
+    Example use:
 
     ```yaml
     mirrored_channels:
