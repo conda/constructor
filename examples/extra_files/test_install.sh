c32f4b96bd365b2651fd2d147ea0b37fa22d9701
randersson <randersson@anaconda.com>
2025-10-09 15:48:52 -0400

    Add test

--- examples/extra_files/test_install.sh
+++ examples/extra_files/test_install.sh
@@ -4,3 +4,7 @@ echo "Added by test-install script" > "$PREFIX/test_install_sentinel.txt"
 
 test -f "$PREFIX/more_data/README.md"
 test -f "$PREFIX/something2.txt"
+# Ideally we should test the .pkg and .sh installers separately since
+# the current behavior for .sh-installers is to include but also rename the license file to LICENSE.txt,
+# but for macOS the name of the provided license file remains unchanged.
+test -f "$PREFIX/LICENSE.txt"
