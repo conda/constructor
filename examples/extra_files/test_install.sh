99e6cef14a65f0051b77a90ba8f78ebd6e2b31a1
Robin <34315751+lrandersson@users.noreply.github.com>
2025-10-21 17:37:15 -0400

    Add license file to .pkg installers (#1085)
    * Add license file to .pkg installers
    
    * Add test
    
    * Add news
    
    * Rename file, test difference between .sh/.pkg
    
    * Updated test to point at which file that is missing
    
    * improve error for file copy operation, fix issue in windows test
    
    * Update from pre-commit
    
    * Fix issue with string being passed instead of a list
    
    * add fixes from review

--- examples/extra_files/test_install.sh
+++ examples/extra_files/test_install.sh
@@ -2,5 +2,33 @@
 set -euxo pipefail
 echo "Added by test-install script" > "$PREFIX/test_install_sentinel.txt"
 
-test -f "$PREFIX/more_data/README.md"
-test -f "$PREFIX/something2.txt"
+missing=false
+
+if [ ! -f "$PREFIX/more_data/README.md" ]; then
+    echo "Missing: $PREFIX/more_data/README.md"
+    missing=true
+fi
+
+if [ ! -f "$PREFIX/something2.txt" ]; then
+    echo "Missing: $PREFIX/something2.txt"
+    missing=true
+fi
+
+# Ideally we should test the .pkg and .sh installers separately since
+# the current behavior for .sh-installers is to include but also rename the license file to LICENSE.txt,
+# but for .pkg the name of the provided license file remains unchanged.
+if [ "$INSTALLER_TYPE" = "SH" ]; then
+    if [ ! -f "$PREFIX/LICENSE.txt" ]; then
+        echo "Missing: $PREFIX/LICENSE.txt"
+        missing=true
+    fi
+else  # .pkg
+    if [ ! -f "$PREFIX/TEST_LICENSE.txt" ]; then
+        echo "Missing: $PREFIX/TEST_LICENSE.txt"
+        missing=true
+    fi
+fi
+
+if [ "$missing" = true ]; then
+    exit 1
+fi
