7e7b8bc5be0f759052c751d7d4de0d48696f7b69
Robin <randersson@anaconda.com>
2025-11-25 16:01:43 -0500

    Add Install Options Page - WIP

--- constructor/briefcase.py
+++ constructor/briefcase.py
@@ -114,6 +114,33 @@ def get_license(info):
     placeholder_license = Path(__file__).parent / "nsis" / "placeholder_license.txt"
     return {"file": str(placeholder_license)}  # convert to str for TOML serialization
 
+def create_install_options_list(info: dict) -> list[dict]:
+    """Returns a list of dicts with data formatted for the installation options page."""
+    options = []
+    register_python = info.get("register_python", True)
+    if register_python:
+        options.append(
+            {
+                "name": "register_python",
+                "title": "Register Python as System Default",
+                "description": "TODO: Register Python description",
+                "default": info.get("register_python_default", False),
+            }
+        )
+    initialize_conda = info.get("initialize_conda", "classic")
+    if initialize_conda:
+        # TODO: How would we distinguish between True/classic in the UI? Same for NSIS
+        options.append(
+            {
+                "name": "initialize_conda",
+                "title": "Initialize Conda",
+                "description": "TODO: Initialize conda description",
+                "default": info.get("initialize_by_default", False),
+            }
+        )
+
+    return options
+
 
 # Create a Briefcase configuration file. Using a full TOML writer rather than a Jinja
 # template allows us to avoid escaping strings everywhere.
@@ -134,6 +161,7 @@ def write_pyproject_toml(tmp_dir, info):
                 "use_full_install_path": False,
                 "install_launcher": False,
                 "post_install_script": str(BRIEFCASE_DIR / "run_installation.bat"),
+                "install_option": create_install_options_list(info),
             }
         },
     }
