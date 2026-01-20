cf05a36af5cbb0d001b2fa265c7bd31b17ec6325
Robin <34315751+lrandersson@users.noreply.github.com>
2026-01-20 17:29:02 -0500

    Briefcase installer options (#1144)
    * Add Install Options Page - WIP
    
    * Review fixes
    
    * Fix description
    
    * Remove comment and fix format
    
    * Review fixes
    
    * Remove 'self'
    
    * Review fixes

--- constructor/briefcase.py
+++ constructor/briefcase.py
@@ -115,6 +115,109 @@ def get_license(info):
     return {"file": str(placeholder_license)}  # convert to str for TOML serialization
 
 
+def is_bat_file(file_path: Path) -> bool:
+    return file_path.is_file() and file_path.suffix.lower() == ".bat"
+
+
+def create_install_options_list(info: dict) -> list[dict]:
+    """Returns a list of dicts with data formatted for the installation options page."""
+    options = []
+
+    # Register Python (if Python is bundled)
+    has_python = False
+    for item in info.get("_dists", []):
+        if item.startswith("python-"):
+            components = item.split("-")  # python-x.y.z-<build number>.suffix
+            python_version = ".".join(components[1].split(".")[:-1])  # create the string "x.y"
+            has_python = True
+            break
+
+    if has_python and info.get("register_python", True):
+        options.append(
+            {
+                "name": "register_python",
+                "title": f"Register {info['name']} as my default Python {python_version}.",
+                "description": "Allows other programs, such as VSCode, PyCharm, etc. to automatically "
+                f"detect {info['name']} as the primary Python {python_version} on the system.",
+                "default": info.get("register_python_default", False),
+            }
+        )
+
+    # Initialize conda
+    initialize_conda = info.get("initialize_conda", "classic")
+    if initialize_conda:
+        if initialize_conda == "condabin":
+            description = (
+                "Adds condabin, which only contains the 'conda' executables, to PATH. "
+                "Does not require special shortcuts but activation needs "
+                "to be performed manually."
+            )
+        else:
+            description = (
+                "NOT recommended. This can lead to conflicts with other applications. "
+                "Instead, use the Command Prompt and Powershell menus added to the Windows Start Menu."
+            )
+        options.append(
+            {
+                "name": "initialize_conda",
+                "title": "Add installation to my PATH environment variable",
+                "description": description,
+                "default": info.get("initialize_by_default", False),
+            }
+        )
+
+    # Keep package option (presented to the user as a negation (clear package cache))
+    clear_package_cache = not info.get("keep_pkgs", False)
+    options.append(
+        {
+            "name": "clear_package_cache",
+            "title": "Clear the package cache upon completion",
+            "description": "Recommended. Recovers some disk space without harming functionality.",
+            "default": clear_package_cache,
+        }
+    )
+
+    # Enable shortcuts
+    if info.get("_enable_shortcuts", False) is True:
+        options.append(
+            {
+                "name": "enable_shortcuts",
+                "title": "Create shortcuts",
+                "description": "Create shortcuts (supported packages only).",
+                "default": False,
+            }
+        )
+
+    # Pre/Post install script
+    for script_type in ["pre", "post"]:
+        script_description = info.get(f"{script_type}_install_desc", "")
+        script = info.get(f"{script_type}_install", "")
+        if script_description and not script:
+            raise ValueError(
+                f"{script_type}_install_desc was set, but {script_type}_install was not!"
+            )
+
+        if script:
+            script_path = Path(script)
+            if not is_bat_file(script_path):
+                raise ValueError(
+                    f"Specified {script_type}-install script '{script}' must be an existing '.bat' file."
+                )
+
+        # The UI option is only displayed if a description is set
+        if script_description:
+            options.append(
+                {
+                    "name": f"{script_type}_install_script",
+                    "title": f"{script_type.capitalize()}-install script",
+                    "description": script_description,
+                    "default": False,
+                }
+            )
+
+    return options
+
+
 # Create a Briefcase configuration file. Using a full TOML writer rather than a Jinja
 # template allows us to avoid escaping strings everywhere.
 def write_pyproject_toml(tmp_dir, info):
@@ -134,6 +237,7 @@ def write_pyproject_toml(tmp_dir, info):
                 "use_full_install_path": False,
                 "install_launcher": False,
                 "post_install_script": str(BRIEFCASE_DIR / "run_installation.bat"),
+                "install_option": create_install_options_list(info),
             }
         },
     }
