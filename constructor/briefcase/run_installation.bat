7eaf03a39ebbbd05d7da990914bd9cb19890dc28
Robin <randersson@anaconda.com>
2026-01-27 14:07:35 -0500

    Work in progress

--- constructor/briefcase/run_installation.bat
+++ constructor/briefcase/run_installation.bat
@@ -1,11 +1,18 @@
-set "PREFIX=%cd%"
-set "BASE_PATH=%PREFIX%\base"
-"%PREFIX%\_conda" constructor --prefix "%BASE_PATH%" --extract-conda-pkgs
+set "INSTDIR=%cd%"
+set "BASE_PATH=%INSTDIR%\base"
+set "PREFIX=%BASE_PATH%"
+set "CONDA_EXE=%INSTDIR%\_conda.exe"
+
+set "MENUINST_BASE_PREFIX=%INSTDIR%"
+set "MENUINST_PREFIX=%INSTDIR%"
+"%INSTDIR%\_conda.exe" constructor --prefix "%BASE_PATH%" --extract-conda-pkgs
 
 set CONDA_PROTECT_FROZEN_ENVS=0
-set "CONDA_ROOT_PREFIX=%PREFIX%"
+set "CONDA_ROOT_PREFIX=%BASE_PATH%"
 set CONDA_SAFETY_CHECKS=disabled
 set CONDA_EXTRA_SAFETY_CHECKS=no
-set CONDA_PKGS_DIRS=%BASE_PATH%\pkgs
+set "CONDA_PKGS_DIRS=%BASE_PATH%\pkgs"
 
-"%PREFIX%\_conda" install --offline --file "%BASE_PATH%\conda-meta\initial-state.explicit.txt" -yp "%BASE_PATH%"
+"%INSTDIR%\_conda.exe" install --offline --file "%BASE_PATH%\conda-meta\initial-state.explicit.txt" -yp "%BASE_PATH%"
+echo "Pausing..."
+pause
