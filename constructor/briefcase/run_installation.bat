1d82e26bb1e47765bb57547faee1b5949faf78fa
Robin <34315751+lrandersson@users.noreply.github.com>
2026-02-02 09:28:17 -0500

    MSI: Change installer layout (#1160)
    * Work in progress
    
    * Work in progress
    
    * Remove unnecessary packages
    
    * Remove call to 'pause' that was used for debugging
    
    * Fix grammar in error message
    
    Co-authored-by: Marco Esters <mesters@anaconda.com>
    
    ---------
    
    Co-authored-by: Marco Esters <mesters@anaconda.com>

--- constructor/briefcase/run_installation.bat
+++ constructor/briefcase/run_installation.bat
@@ -1,10 +1,14 @@
-set "PREFIX=%cd%"
-_conda constructor --prefix "%PREFIX%" --extract-conda-pkgs
+set "INSTDIR=%cd%"
+set "BASE_PATH=%INSTDIR%\base"
+set "PREFIX=%BASE_PATH%"
+set "CONDA_EXE=%INSTDIR%\_conda.exe"
+
+"%INSTDIR%\_conda.exe" constructor --prefix "%BASE_PATH%" --extract-conda-pkgs
 
 set CONDA_PROTECT_FROZEN_ENVS=0
-set CONDA_ROOT_PREFIX=%PREFIX%
+set "CONDA_ROOT_PREFIX=%BASE_PATH%"
 set CONDA_SAFETY_CHECKS=disabled
 set CONDA_EXTRA_SAFETY_CHECKS=no
-set CONDA_PKGS_DIRS=%PREFIX%\pkgs
+set "CONDA_PKGS_DIRS=%BASE_PATH%\pkgs"
 
-_conda install --offline --file "%PREFIX%\conda-meta\initial-state.explicit.txt" -yp "%PREFIX%"
+"%INSTDIR%\_conda.exe" install --offline --file "%BASE_PATH%\conda-meta\initial-state.explicit.txt" -yp "%BASE_PATH%"
