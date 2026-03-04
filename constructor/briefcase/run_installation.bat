7793e788b289725d667dca6255af1486ab9d1221
Robin <34315751+lrandersson@users.noreply.github.com>
2026-03-04 14:48:25 -0500

    MSI: Reorganize payload preparation (#1164)
    * Reorganize payload into a class and add tests
    
    * Ensure new tests are Windows only
    
    * Add jinja templating, payload as tar, tests
    
    * Update docstring
    
    * Update briefcase.py
    
    * Inline test utility function
    
    * Remove payload tar, update pre_uninstall.bat
    
    * Use conda-standalone for extracting tar
    
    * Merge archive functions, update root as cached property
    
    * Remove template_file.py improve handling of templates
    
    * Add missing .dst
    
    * Remove compresslevel arg
    
    * Review fixes
    
    * Fix typo in file name causing build errors
    
    * Dynamically set archive type from file name
    
    * Rename class function and update docstring
    
    * Update uninstallation scripts
    
    * Update pre_uninstall.bat
    
    * Add logging
    
    * Improve log handling for msi tests
    
    * Add register_envs
    
    * Fix syntax error with remove
    
    * Ensure .exe test not running for MSI
    
    * Properly disable test for MSI
    
    * Add more tests and another check errorlevel
    
    * Make logging more neat
    
    * Removed all use of 'sanity'
    
    * Updated test for MSI (remove pytest.skip)
    
    * Update to use CLI for newer conda-standalone
    
    * Fix missing quote and properly use --log-file
    
    * Docstring formatting
    
    * pre-commit fix
    
    * Hopefully fix issue with conda-standalone canary
    
    * Fix typo in workflow
    
    * Automatically create 'dst'
    
    * Remove TemplateFile
    
    * Fix docstring
    
    * FIx pre-commit
    
    * Always log to file
    
    * Generalize install/uninstall logs and move into INSTDIR
    
    * pre-commit fix
    
    * Remove PayloadLayout
    
    * Fix remaining syntax error
    
    * Review fixes

--- constructor/briefcase/run_installation.bat
+++ constructor/briefcase/run_installation.bat
@@ -1,14 +1,68 @@
-set "INSTDIR=%cd%"
+@echo {{ 'on' if add_debug else 'off' }}
+setlocal
+
+{% macro error_block(message, code) %}
+echo [ERROR] {{ message }}
+>> "%LOG%" echo [ERROR] {{ message }}
+exit /b {{ code }}
+{% endmacro %}
+
+rem Assign INSTDIR and normalize the path
+set "INSTDIR=%~dp0.."
+for %%I in ("%INSTDIR%") do set "INSTDIR=%%~fI"
+
 set "BASE_PATH=%INSTDIR%\base"
 set "PREFIX=%BASE_PATH%"
-set "CONDA_EXE=%INSTDIR%\_conda.exe"
+set "CONDA_EXE=%INSTDIR%\{{ conda_exe_name }}"
+set "PAYLOAD_TAR=%INSTDIR%\{{ archive_name }}"
 
-"%INSTDIR%\_conda.exe" constructor --prefix "%BASE_PATH%" --extract-conda-pkgs
-
-set CONDA_PROTECT_FROZEN_ENVS=0
-set "CONDA_ROOT_PREFIX=%BASE_PATH%"
-set CONDA_SAFETY_CHECKS=disabled
 set CONDA_EXTRA_SAFETY_CHECKS=no
+set CONDA_PROTECT_FROZEN_ENVS=0
+set CONDA_REGISTER_ENVS={{ register_envs }}
+set CONDA_SAFETY_CHECKS=disabled
+set "CONDA_ROOT_PREFIX=%BASE_PATH%"
 set "CONDA_PKGS_DIRS=%BASE_PATH%\pkgs"
 
-"%INSTDIR%\_conda.exe" install --offline --file "%BASE_PATH%\conda-meta\initial-state.explicit.txt" -yp "%BASE_PATH%"
+rem Get the name of the install directory
+for %%I in ("%INSTDIR%") do set "APPNAME=%%~nxI"
+set "LOG=%INSTDIR%\install.log"
+
+{%- if add_debug %}
+echo ==== run_installation start ==== >> "%LOG%"
+echo SCRIPT=%~f0 >> "%LOG%"
+echo CWD=%CD% >> "%LOG%"
+echo INSTDIR=%INSTDIR% >> "%LOG%"
+echo BASE_PATH=%BASE_PATH% >> "%LOG%"
+echo CONDA_EXE=%CONDA_EXE% >> "%LOG%"
+echo PAYLOAD_TAR=%PAYLOAD_TAR% >> "%LOG%"
+{%- endif %}
+
+rem Consistency checks
+if not exist "%CONDA_EXE%" (
+  {{ error_block('CONDA_EXE not found: "%CONDA_EXE%"', 10) }}
+)
+if not exist "%PAYLOAD_TAR%" (
+  {{ error_block('PAYLOAD_TAR not found: "%PAYLOAD_TAR%"', 11) }}
+)
+
+echo Unpacking payload...
+"%CONDA_EXE%" --log-file "%LOG%" constructor extract --prefix "%INSTDIR%" --tar-from-stdin < "%PAYLOAD_TAR%"
+if errorlevel 1 ( exit /b %errorlevel% )
+
+"%CONDA_EXE%" --log-file "%LOG%" constructor extract --prefix "%BASE_PATH%" --conda-pkgs
+if errorlevel 1 ( exit /b %errorlevel% )
+
+if not exist "%BASE_PATH%" (
+  {{ error_block('"%BASE_PATH%" not found!', 12) }}
+)
+
+"%CONDA_EXE%" --log-file "%LOG%" install --offline --file "%BASE_PATH%\conda-meta\initial-state.explicit.txt" -yp "%BASE_PATH%"
+if errorlevel 1 ( exit /b %errorlevel% )
+
+rem Delete the payload to save disk space.
+rem A truncated placeholder of 0 bytes is recreated during uninstall
+rem because MSI expects the file to be there to clean the registry.
+del "%PAYLOAD_TAR%"
+if errorlevel 1 ( exit /b %errorlevel% )
+
+exit /b 0
