ec455fa5fd219ac31af1986c8c5ccd8490d93167
Jaida Rice <jaidarice@gmail.com>
2026-05-06 07:48:00 -0700

    Update docker_build.py

--- constructor/docker_build.py
+++ constructor/docker_build.py
@@ -2,6 +2,7 @@ import logging
 import shutil
 import subprocess
 import tempfile
+import platform
 from pathlib import Path
 from jinja2 import Template
 
@@ -11,32 +12,43 @@ logger = logging.getLogger(__name__)
 
 TEMPLATE_PATH = Path(__file__).parent / "dockerfile_template.tmpl"
 
-def prepare_docker_context(info: dict) -> tuple[Path, Path]:
+DOCKER_PLATFORM_MAP = {
+    "linux-64": "linux/amd64",
+    "linux-aarch64": "linux/arm64",
+    "linux-armv7l": "linux/arm/v7",
+    "linux-32": "linux/386",
+    "linux-ppc64le": "linux/ppc64le",
+    "linux-s390x": "linux/s390x",
+    "osx-arm64": "linux/arm64",
+    "osx-64": "linux/amd64",
+}
+
+def prepare_docker_context(info: dict, tmp_dir: Path) -> tuple[Path, Path]:
     """Copy the .sh installer into the Docker build directory.
 
     Parameters
     ----------
     info: dict
-        Constructor installer info dict. Must contain ``_outpath`` and ``_output_dir`` pointing to the built .sh installer and output directory respectively.
+        Constructor installer info dict. Must contain ``_outpath`` and ``_output_dir`` pointing to the built .sh
+        installer and output directory respectively.
+    tmp_dir: Path
+        Path to a temporary directory to stage the Docker build context. The .sh installer will be copied to this directory.
 
     Returns
     -------
     Path
-        Path to the Docker build directory (``<_output_dir>/docker``).
+        Path to the tmp Docker build directory (``<_output_dir>/docker``).
     """
-    docker_dir = Path(info["_output_dir"]) / "docker"
-    docker_dir.mkdir(parents=True, exist_ok=True)
-
     installer_path = Path(info["_outpath"])
     if not installer_path.exists():
         raise RuntimeError(
             f"Expected .sh installer not found: {installer_path}\n"
         )
 
-    shutil.copy(installer_path, docker_dir / installer_path.name)
-    logger.info("Copied installer to Docker directory: %s", docker_dir / installer_path.name)
+    shutil.copy(installer_path, tmp_dir / installer_path.name)
+    logger.info("Copied installer to tmp directory: %s", tmp_dir / installer_path.name)
 
-    return docker_dir
+    return tmp_dir
 
 
 def generate_dockerfile(info: dict, docker_dir: Path) -> Path:
@@ -55,6 +67,10 @@ def generate_dockerfile(info: dict, docker_dir: Path) -> Path:
     Path
         Path to the generated Dockerfile. ``<docker_dir>/Dockerfile``.
     """
+    from .conda_interface import MatchSpec
+
+    specs = {MatchSpec(spec).name for spec in info.get("specs", ())}
+
     docker_template = Template(TEMPLATE_PATH.read_text())
 
     docker_base_image = info.get("docker_base_image")
@@ -62,6 +78,12 @@ def generate_dockerfile(info: dict, docker_dir: Path) -> Path:
         raise RuntimeError(
             "Base image for Dockerfile not specified. "
             "Please set 'docker_base_image' in construct.yaml, e.g.:\n"
+            " docker_base_image: debian:13.4-slim@sha256:4ffb3a1511099754cddc70eb1b12e50ffdb67619aa0ab6c13fcd800a78ef7c7a\n"
+        )
+    if "@" not in docker_base_image:
+        logger.warning(
+            "No SHA256 digest specified for docker_base_image. "
+            "Consider specifying a digest to ensure the integrity of the base image used for the build, e.g.:\n"
             " docker_base_image: debian:13.4-slim@sha256:4ffb3a1511099754cddc70eb1b12e50ffdb67619aa0ab6c13fcd800a78ef7c7a\n"
         )
 
@@ -73,6 +95,9 @@ def generate_dockerfile(info: dict, docker_dir: Path) -> Path:
         name=info["name"],
         version=info["version"],
         labels=info.get("docker_labels", {}),
+        init_cmd = "$PREFIX/bin/mamba shell" if "mamba" in specs else "$PREFIX/bin/python -m conda",
+        register_envs=info.get("register_envs", True),
+        keep_pkgs=info.get("keep_pkgs", False),
     )
 
     dockerfile_path = docker_dir / "Dockerfile"
@@ -93,59 +118,38 @@ def build_image(info: dict, docker_dir: Path) -> None:
         Path to the Docker directory containing the Dockerfile.
 
     """
+    if info.get("_platform") not in DOCKER_PLATFORM_MAP:
+        logger.warning(
+            f"Building Docker images is not supported on platform '{info['_platform']}'. "
+            "Skipping Docker build. You can still generate the Dockerfile by and build the image manually using 'docker buildx' on a supported platform or using Docker Desktop. "
+            "Supported platforms for Docker build are: linux/amd64 and linux/arm64."
+        )
+        return
+
     if shutil.which("docker") is None:
         raise RuntimeError(
             "Building a Docker image requires the 'docker' CLI tool to be installed and available in PATH. "
             "Install Docker Desktop or Docker Engine to proceed, or "
-            "use `installer_type: docker  # [linux]` in construct.yaml to "
+            "use `installer_type: docker` in construct.yaml to "
             "generate the Dockerfile without building the image."
         )
 
-    osname, arch = info["_platform"].split("-")
-
-    if osname == "linux" and arch in ("amd64", "arm64"):
-       logger.info(f"Building Docker image on supported platform: {info['_platform']}")
-    elif osname == "linux" and arch not in ("amd64", "arm64"):
-        logger.warning(
-            f"Building Docker images on linux/{arch} is not fully supported. "
-            "Tread carefully as the resulting image may not work as expected. "
-            "The resulting image may fail due to architecture incompatibility. "
-    else:
+    docker_platform = DOCKER_PLATFORM_MAP.get(info["_platform"])
+    if docker_platform is None:
         raise RuntimeError(
-            f"Unsupported architecture for Docker build: {info['_platform']}\n"
-            "Currently, building Docker images is only supported on linux/amd64 and linux/arm64 platforms. "
-            "Please run the build on a Linux platform or alternatively, "
-            "use `installer_type: docker  # [linux]` in construct.yaml to "
-            "generate the Dockerfile without building the image. Then you can build the Docker image manually using 'docker buildx' on non-Linux platforms. "
+            f"Unsupported platform for Docker build: '{info['_platform']}'. "
+            "Supported platforms are: {', '.join(DOCKER_PLATFORM_MAP)}."
         )
 
     image_name = info.get("docker_image_name", info["name"].lower())
     image_version = info.get("docker_image_version", info["version"].split("-")[0])
     tag = f"{image_name}:{image_version}"
 
-    cmd = ["docker", "build", "-t", tag, str(docker_dir)]
+    cmd = ["docker", "buildx", "build", "--load", "--platform", docker_platform, "-t", tag, str(docker_dir)]
 
     logger.info("Building Docker image: '%s'", tag)
     subprocess.run(cmd, check=True)
     logger.info("Docker image built: '%s'", tag)
-
-def cleanup(docker_dir: Path, info: dict) -> None:
-    """Copy final artifacts to output directory and clean up temporary files.
-
-    Parameters
-    ----------
-    docker_dir: Path
-        Final output directory containing the Dockerfile. (``<_output_dir>/docker``)
-    info: dict
-        Constructor installer info dict.
-
-    """
-    if build_image == success:
-        installer_path.unlink(missing_ok=True)
-        logger.info("Removing installer from paths: %s, %s", installer_path, docker_dir.joinpath(installer_path.name))
-
-    # TODO: Add option for agressive cleanup which would remove dockerfile and sh installer if building is enabled.
-
 
 def create(info: dict, verbose: bool = False) -> None:
     """Build a Docker output
@@ -159,12 +163,17 @@ def create(info: dict, verbose: bool = False) -> None:
         Defaults to ``False``.
 
     """
-    docker_dir = prepare_docker_context(info)
-    generate_dockerfile(info, docker_dir)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        tmp_path = Path(temp_dir)
+        prepare_docker_context(info, tmp_path)
+        generate_dockerfile(info, tmp_path)
 
-    if info.get("docker_build"):
-        build_image(info, docker_dir)
+        if info.get("docker_build"):
+            build_image(info, tmp_path)
 
-    cleanup(info, docker_dir)
+        output_docker_dir = Path(info["_output_dir"]) / "docker"
+        output_docker_dir.mkdir(parents=True, exist_ok=True)
+        shutil.copy(tmp_path / "Dockerfile", output_docker_dir / "Dockerfile")
+        shutil.copy(tmp_path / Path(info["_outpath"]).name, output_docker_dir / Path(info["_outpath"]).name)
 
-    logger.info("Docker output complete. Docker directory: '%s'", docker_dir)
+    logger.info("Docker output complete. Docker directory: '%s'", output_docker_dir)
