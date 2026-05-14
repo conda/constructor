583191d69c6737b23088ede5fd935f0d8cb9daba
Jaida Rice <100002667+Jrice1317@users.noreply.github.com>
2026-05-14 08:44:34 -0700

    Apply suggestions from code review
    Co-authored-by: Marco Esters <mesters@anaconda.com>

--- constructor/docker_build.py
+++ constructor/docker_build.py
@@ -21,8 +21,6 @@ DOCKER_PLATFORM_MAP = {
     "linux-32": "linux/386",
     "linux-ppc64le": "linux/ppc64le",
     "linux-s390x": "linux/s390x",
-    "osx-arm64": "linux/arm64",
-    "osx-64": "linux/amd64",
 }
 
 
@@ -44,7 +42,7 @@ def prepare_docker_context(info: dict, tmp_dir: Path) -> Path:
     """
     installer_path = Path(info["_outpath"])
     if not installer_path.exists():
-        raise RuntimeError(f"Expected .sh installer not found: {installer_path}\n")
+        raise RuntimeError(f"Expected .sh installer not found: {installer_path}")
 
     shutil.copy(installer_path, tmp_dir / installer_path.name)
     logger.info("Copied installer to tmp directory: %s", tmp_dir / installer_path.name)
