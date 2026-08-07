accdd549b9b960ca8587b077bc7d20492d9e06e4
Robin <randersson@anaconda.com>
2026-08-07 12:13:47 -0400

    DEBUG: Temporarily disable call to new function

--- constructor/build_outputs.py
+++ constructor/build_outputs.py
@@ -86,7 +86,7 @@ def dump_info(info):
             return repr(obj)
 
     # Packages installed in the environment running constructor.
-    info["_build_environment_packages"] = get_build_env_records()
+    # info["_build_environment_packages"] = get_build_env_records()
     outpath = os.path.join(info["_output_dir"], "info.json")
     with open(outpath, "w") as f:
         json.dump(info, f, indent=2, default=_serialize)
